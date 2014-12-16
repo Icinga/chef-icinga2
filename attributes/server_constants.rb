416e4bbb1cc6c18f7bb1597573506e0ef42860a7
vkhatri <vir.khatri@gmail.com>
2014-12-16 19:58:48 +0530

    added icinga2 server classic ui and objects default attrs file

---
+++ attributes/server_constants.rb
@@ -0,0 +1,4 @@
+
+default['icinga2']['server']['constants']['PluginDir'] = node['icinga2']['plugins_dir']
+default['icinga2']['server']['constants']['ManubulonPluginDir'] = node['icinga2']['plugins_dir']
+default['icinga2']['server']['constants']['TicketSalt'] = 'ch2ng3m3'
