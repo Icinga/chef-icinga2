d6881a6fd5b861c3687357b84079126898c76267
vkhatri <vir.khatri@gmail.com>
2014-12-16 22:30:36 +0530

    added icinga2_client examples

---
+++ examples/icinga2_client/attributes/default.rb
@@ -0,0 +1,11 @@
+default['icinga2']['client']['nrpe']['checks']['warning']['cpu_load'] = 85
+default['icinga2']['client']['nrpe']['checks']['critical']['cpu_load'] = 100
+
+default['icinga2']['client']['nrpe']['checks']['warning']['free_disk_percentage'] = 20
+default['icinga2']['client']['nrpe']['checks']['critical']['free_disk_percentage'] = 10
+
+default['icinga2']['client']['nrpe']['checks']['warning']['free_memory_percentage'] = 20
+default['icinga2']['client']['nrpe']['checks']['critical']['free_memory_percentage'] = 10
+
+default['icinga2']['client']['nrpe']['checks']['warning']['zombie_count'] = 5
+default['icinga2']['client']['nrpe']['checks']['critical']['zombie_count'] = 10
