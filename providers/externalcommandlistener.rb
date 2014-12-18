f3441f27a2c6eff6b68e90345d7266f3a74f7cd7
vkhatri <vir.khatri@gmail.com>
2014-12-18 14:10:33 +0530

    added externalcommandlistener.rb lwrp

---
+++ providers/externalcommandlistener.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: icinga2
+# Provider:: externalcommandlistener
+#
+# Copyright 2014, Virender Khatri
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+def whyrun_supported?
+  true
+end
+
+action :create do
+  new_resource.updated_by_last_action(true) if object_template
+end
+
+action :delete do
+  new_resource.updated_by_last_action(true) if object_template
+end
+
+protected
+
+# create object resource
+def object_template
+  ot = template ::File.join(node['icinga2']['objects_dir'], "#{::File.basename(__FILE__, '.rb')}.conf") do
+    source "object.#{::File.basename(__FILE__, '.rb')}.conf.erb"
+    cookbook 'icinga2'
+    owner node['icinga2']['user']
+    group node['icinga2']['group']
+    mode 0640
+    variables(:object => new_resource.name,
+              :library => new_resource.library,
+              :command_path => new_resource.command_path)
+    notifies :reload, 'service[icinga2]', :delayed
+  end
+  ot.updated?
+end
