82ad866d184de9c893a039326a39b41fae439907
vkhatri <vir.khatri@gmail.com>
2015-01-04 21:51:28 +0530

    removed unused attribute

--- attributes/server_web2.rb
+++ attributes/server_web2.rb
@@ -5,5 +5,4 @@ default['icinga2']['web2']['version'] = 'master'
 default['icinga2']['web2']['web_root'] = '/usr/share/icingaweb2'
 default['icinga2']['web2']['web_uri'] = '/icingaweb'
 default['icinga2']['web2']['conf_dir'] = '/etc/icingaweb2'
-default['icinga2']['web2']['conf_d_dir'] = ::File.join(node['icinga2']['web2']['conf_dir'], 'conf.d')
 default['icinga2']['web2']['log_dir'] = '/var/log/icingaweb2'
