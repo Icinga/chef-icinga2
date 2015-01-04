79015813d3f5761a6f721eb40ff059a919ca2420
vkhatri <vir.khatri@gmail.com>
2015-01-04 21:23:20 +0530

    disabling ido schema load by default

--- attributes/server_ido.rb
+++ attributes/server_ido.rb
@@ -1,6 +1,6 @@
 
 default['icinga2']['ido']['type'] = 'mysql'
-default['icinga2']['ido']['load_schema'] = true
+default['icinga2']['ido']['load_schema'] = false
 default['icinga2']['ido']['db_host'] = 'localhost'
 default['icinga2']['ido']['db_name'] = 'icinga'
 default['icinga2']['ido']['db_user'] = 'icinga'
