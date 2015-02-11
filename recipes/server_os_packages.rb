e389a460c44ea740d29aa38a8ea991a15ad7b7c6
Virender Khatri <vir.khatri@gmail.com>
2015-02-12 00:16:43 +0530

    adding back package php-pecl-imagick for amazon platform

--- recipes/server_os_packages.rb
+++ recipes/server_os_packages.rb
@@ -24,7 +24,13 @@ when 'debian'
   os_packages = %w(g++ mailutils php5 php5-cli php5-fpm build-essential libgd2-xpm-dev libjpeg62 libjpeg62-dev libpng12-0 libpng12-dev libapache2-mod-php5 git-core)
   include_recipe 'apt'
 when 'rhel'
-  os_packages = %w(gcc gcc-c++ glibc glibc-common mailx php php-devel gd gd-devel libjpeg libjpeg-devel libpng libpng-devel php-gd php-fpm php-cli php-pear php-xmlrpc php-xsl php-pdo php-soap php-ldap php-mysql php-pgsql php-intl git ImageMagick ImageMagick-devel)
+  os_packages = %w(gcc gcc-c++ glibc glibc-common mailx php php-devel gd gd-devel libjpeg libjpeg-devel libpng libpng-devel php-gd php-fpm php-cli php-pear php-xmlrpc php-xsl php-pdo php-soap php-ldap php-mysql php-pgsql php-intl git)
+  case node['platform']
+  when 'redhat','centos','fedora'
+    os_packages += %w(ImageMagick ImageMagick-devel)
+  when 'amazon'
+    os_packages.push 'php-pecl-imagick'
+  end
 end
 
 # dependencies
