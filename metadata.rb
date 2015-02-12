a891ce1dab6ea12d922875ffb8704164f52dd431
Virender Khatri <vir.khatri@gmail.com>
2015-02-12 23:03:00 +0530

    bump cookbook version to v2.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vir.khatri@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures Icinga2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.10.1'
+version '2.0.1'
 
 depends 'apt'
 depends 'yum', '~> 3.0'
