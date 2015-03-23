ec228ab494d81d29bc94578efcd686de62b2207c
Virender Khatri <vir.khatri@gmail.com>
2015-03-23 07:39:25 +0530

    Feature #8839, bump icingaweb2 ref to beta3

--- attributes/server_web2.rb
+++ attributes/server_web2.rb
@@ -1,7 +1,7 @@
 
 default['icinga2']['web2']['enable'] = false
 default['icinga2']['web2']['source_url'] = 'git://git.icinga.org/icingaweb2.git'
-default['icinga2']['web2']['version'] = 'v2.0.0-beta2'
+default['icinga2']['web2']['version'] = 'v2.0.0-beta3'
 default['icinga2']['web2']['web_root'] = '/usr/share/icingaweb2'
 default['icinga2']['web2']['web_uri'] = '/icingaweb2'
 default['icinga2']['web2']['conf_dir'] = '/etc/icingaweb2'
