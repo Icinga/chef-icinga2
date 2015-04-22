b1f1f1c82c61a8e167ae12899630bb09b1fcab6f
Virender Khatri <vir.khatri@gmail.com>
2015-04-22 12:54:21 +0530

    iBug #9151, bump classic_ui version to v1.13.2

--- attributes/server_classic_ui.rb
+++ attributes/server_classic_ui.rb
@@ -6,8 +6,8 @@ default['icinga2']['classic_ui']['apache_conf'] = value_for_platform(
   %w(debian ubuntu) => { 'default' => 'icinga2-classicui' }
 )
 
-default['icinga2']['classic_ui']['version'] = '2.3.3-1'
-default['icinga2']['classic_ui']['gui_version'] = '1.12.2-0'
+default['icinga2']['classic_ui']['version'] = '2.3.4-1'
+default['icinga2']['classic_ui']['gui_version'] = '1.13.2-0'
 default['icinga2']['classic_ui']['web_root'] = value_for_platform(
   %w(centos redhat fedora amazon) => { 'default' => '/usr/share/icinga' },
   %w(debian ubuntu) => { 'default' => '/usr/share/icinga2/classicui' }
