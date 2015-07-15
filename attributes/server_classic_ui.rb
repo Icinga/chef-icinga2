f25cfbf45bd1ff938b25e0db82eb464f8b2676eb
Virender Khatri <vir.khatri@gmail.com>
2015-07-15 21:35:36 +0530

    Feature #9639, bump icinga2 version to v2.3.7

--- attributes/server_classic_ui.rb
+++ attributes/server_classic_ui.rb
@@ -8,9 +8,9 @@ default['icinga2']['classic_ui']['apache_conf'] = value_for_platform(
 )
 
 if node['platform_family'] == 'debian'
-  default['icinga2']['classic_ui']['version'] = '2.3.5-1'
+  default['icinga2']['classic_ui']['version'] = '2.3.7-1'
 else
-  default['icinga2']['classic_ui']['version'] = '2.3.5-1'
+  default['icinga2']['classic_ui']['version'] = '2.3.7-1'
 end
 
 default['icinga2']['classic_ui']['gui_version'] = '1.13.2-0'
