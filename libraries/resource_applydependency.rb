6e19cd088c944e476f6076644478c57f6dbc937c
Virender Khatri <vir.khatri@gmail.com>
2015-12-07 02:33:50 +0530

    Bug #10775, Add zone resource to icinga2_applydependency resource

--- libraries/resource_applydependency.rb
+++ libraries/resource_applydependency.rb
@@ -95,6 +95,14 @@ class Chef
         )
       end
 
+      def zone(arg = nil)
+        set_or_return(
+          :zone, arg,
+          :kind_of => String,
+          :default => nil
+        )
+      end
+
       def assign_where(arg = nil)
         set_or_return(
           :assign_where, arg,
@@ -123,7 +131,7 @@ class Chef
         set_or_return(
           :resource_properties, arg,
           :kind_of => Array,
-          :default => %w(object_relation object_type parent_host_name child_host_name parent_service_name child_service_name disable_checks disable_notifications period states assign_where ignore_where)
+          :default => %w(object_relation object_type parent_host_name child_host_name parent_service_name child_service_name disable_checks disable_notifications period states zone assign_where ignore_where)
         )
       end
     end
