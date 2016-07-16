3d8216868131394630d3a56f10c73253eaacf588
Virender Khatri <vir.khatri@gmail.com>
2016-07-16 23:13:12 +0530

    fix amazon linux apache modules

--- attributes/server_apache.rb
+++ attributes/server_apache.rb
@@ -1,6 +1,7 @@
 
 default['icinga2']['apache_modules'] = value_for_platform(
-  %w(amazon debian ubuntu) => { 'default' => %w(default mod_python mod_php5 mod_cgi mod_ssl mod_rewrite mpm_prefork) },
+  %w(amazon) => { 'default' => %w(default mod_python mod_php5 mod_cgi mod_ssl mod_rewrite) },
+  %w(debian ubuntu) => { 'default' => %w(default mod_python mod_php5 mod_cgi mod_ssl mod_rewrite mpm_prefork) },
   %w(centos redhat fedora) => { '>= 7.0' => %w(default mod_wsgi mod_php5 mod_cgi mod_ssl mod_rewrite),
                                 'default' => %w(default mod_python mod_php5 mod_cgi mod_ssl mod_rewrite) }
 )
