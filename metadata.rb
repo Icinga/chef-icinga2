fdb4e09ae3423d5b45f7433d68a8008bd9da02b6
Virender Khatri <vir.khatri@gmail.com>
2016-07-20 12:22:48 +0530

    bump version to 2.8.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vir.khatri@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures Icinga2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.8.0'
+version '2.8.1'
 
 source_url 'https://github.com/Icinga/chef-icinga2' if respond_to?(:source_url)
 issues_url 'https://dev.icinga.org/projects/chef-icinga2' if respond_to?(:issues_url)
