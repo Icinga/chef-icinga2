a0555ae8e802045c1caae7e97667b06f719e1efd
Virender Khatri <vir.khatri@gmail.com>
2016-09-27 23:28:10 +0530

    fix icingaweb2 package source based install

--- attributes/server_web2.rb
+++ attributes/server_web2.rb
@@ -1,7 +1,11 @@
 
 default['icinga2']['web2']['enable'] = false
+default['icinga2']['web2']['install_method'] = 'package' # source
 default['icinga2']['web2']['source_url'] = 'git://git.icinga.org/icingaweb2.git'
-default['icinga2']['web2']['version'] = 'v2.0.0-rc1'
+default['icinga2']['web2']['version'] = '2.3.4'
+default['icinga2']['web2']['release'] = value_for_platform(
+  %w(centos redhat fedora amazon) => { 'default' => '1' }
+)
 default['icinga2']['web2']['web_root'] = '/usr/share/icingaweb2'
 default['icinga2']['web2']['web_uri'] = '/icingaweb2'
 default['icinga2']['web2']['conf_dir'] = '/etc/icingaweb2'
--- recipes/server_web2.rb
+++ recipes/server_web2.rb
@@ -17,16 +17,15 @@
 # limitations under the License.
 #
 
+web2_version = node['icinga2']['web2']['install_method'] == 'source' ? 'v' + node['icinga2']['web2']['version'] : node['icinga2']['web2']['version'] + '-' + node['icinga2']['web2']['release'] + node['icinga2']['icinga2_version_suffix'].to_s
+cli_version = node['icinga2']['web2']['version'] + '-' + node['icinga2']['web2']['release'] + node['icinga2']['icinga2_version_suffix']
+
+puts web2_version
+
 directory node['icinga2']['web2']['conf_dir'] do
   owner node[node['icinga2']['web_engine']]['user']
   group node[node['icinga2']['web_engine']]['group']
   mode '02770'
-end
-
-directory node['icinga2']['web2']['web_root'] do
-  owner node[node['icinga2']['web_engine']]['user']
-  group node[node['icinga2']['web_engine']]['group']
-  mode '0775'
 end
 
 directory node['icinga2']['web2']['log_dir'] do
@@ -38,7 +37,7 @@ end
 # setup token
 unless node['icinga2']['web2'].key?('setup_token')
   require 'securerandom'
-  node.set['icinga2']['web2']['setup_token'] = SecureRandom.base64(12)
+  node.normal['icinga2']['web2']['setup_token'] = SecureRandom.base64(12)
 end
 
 file ::File.join(node['icinga2']['web2']['conf_dir'], 'setup.token') do
@@ -48,9 +47,48 @@ file ::File.join(node['icinga2']['web2']['conf_dir'], 'setup.token') do
   mode 0o660
 end
 
-git node['icinga2']['web2']['web_root'] do
-  repository node['icinga2']['web2']['source_url']
-  revision node['icinga2']['web2']['version']
-  user node[node['icinga2']['web_engine']]['user']
-  group node[node['icinga2']['web_engine']]['group']
+# set php time zone
+php_ini = node['platform_family'] == 'rhel' ? '/etc/php.ini' : '/etc/php5/apache2/php.ini'
+ruby_block 'set php timezone' do
+  block do
+    fe = Chef::Util::FileEdit.new(php_ini)
+    fe.search_file_replace_line(/^;date.timezone =.*/, "date.timezone = #{node['time']['timezone']}")
+    fe.write_file
+  end
 end
+
+# install icingaweb2
+if node['icinga2']['web2']['install_method'] == 'source'
+  package 'icingaweb2' do
+    action :remove
+  end
+
+  directory node['icinga2']['web2']['web_root'] do
+    owner node[node['icinga2']['web_engine']]['user']
+    group node[node['icinga2']['web_engine']]['group']
+    mode '0775'
+  end
+
+  git node['icinga2']['web2']['web_root'] do
+    repository node['icinga2']['web2']['source_url']
+    revision web2_version
+    user node[node['icinga2']['web_engine']]['user']
+    group node[node['icinga2']['web_engine']]['group']
+    only_if { node['icinga2']['web2']['install_method'] == 'source' }
+  end
+
+else
+  package 'icingaweb2' do
+    # skip ubuntu version for now
+    version web2_version if node['platform_famil'] == 'rhel'
+    notifies :restart, 'service[icinga2]', :delayed
+  end
+
+  package 'icingacli' do
+    version cli_version
+    notifies :restart, 'service[icinga2]', :delayed
+    only_if { node['platform_family'] == 'rhel' }
+  end
+end
+
+icinga2_feature 'command'
