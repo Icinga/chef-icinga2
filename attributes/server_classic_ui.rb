d80156198cdc568f6826f7d2c3174d679acdccab
Virender Khatri <vir.khatri@gmail.com>
2016-09-27 23:28:32 +0530

    updated icinga classic ui version

--- attributes/server_classic_ui.rb
+++ attributes/server_classic_ui.rb
@@ -8,7 +8,7 @@ default['icinga2']['classic_ui']['apache_conf'] = value_for_platform(
   %w(debian ubuntu) => { 'default' => 'icinga2-classicui' }
 )
 
-default['icinga2']['classic_ui']['version'] = '2.4.1-1'
+default['icinga2']['classic_ui']['version'] = '2.5.4-1'
 
 default['icinga2']['classic_ui']['gui_version'] = '1.13.3-0'
 default['icinga2']['classic_ui']['web_root'] = value_for_platform(
