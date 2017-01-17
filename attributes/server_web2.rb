7acc916f6e72e7042c58e5f0c6a1d803aceaaec3
Jeroen Jacobs <git@jeroenj.be>
2017-01-17 10:58:10 +0100

    Updates the web2 git source url

--- attributes/server_web2.rb
+++ attributes/server_web2.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 default['icinga2']['web2']['enable'] = false
 default['icinga2']['web2']['install_method'] = 'package' # source
-default['icinga2']['web2']['source_url'] = 'git://git.icinga.org/icingaweb2.git'
+default['icinga2']['web2']['source_url'] = 'https://github.com/Icinga/icingaweb2.git'
 default['icinga2']['web2']['version'] = '2.3.4'
 default['icinga2']['web2']['release'] = value_for_platform(
   %w(centos redhat fedora amazon) => { 'default' => '1' }
