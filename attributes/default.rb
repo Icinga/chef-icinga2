cda0a74a59248f9af089b588412dc25aeba5faaf
Virender Khatri <vir.khatri@gmail.com>
2017-01-23 00:23:02 +0530

    update packages version to latest #259, fix for #245 #242

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 # http://docs.icinga.org/icinga2/latest/doc/module/icinga2/chapter/getting-started#getting-started
 
-default['icinga2']['version'] = '2.5.4-1'
+default['icinga2']['version'] = value_for_platform(
+  %w(centos redhat fedora amazon) => { 'default' => '2.6.0-1' },
+  %w(debian ubuntu) => { 'default' => '2.6.0-2' }
+)
 
 default['icinga2']['enable_env_pki'] = false
 default['icinga2']['ignore_version'] = false
