62be8c8cfa2aa3ba60d7132f18bd9cb93c82d638
Virender Khatri <vir.khatri@gmail.com>
2017-02-06 02:29:15 +0530

    rubocop fix

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -75,11 +75,11 @@ when 'debian'
       default['icinga2']['apt']['key'] = 'http://debmon.org/debmon/repo.key'
 
       # icinga2 package version suffix
-      if node['platform_version'] > '7'
-        default['icinga2']['icinga2_version_suffix'] = '~debmon' + node['platform_version'].split('.')[0] + '+1'
-      else
-        default['icinga2']['icinga2_version_suffix'] = '~debmon' + node['platform_version'].split('.')[0] + '0+1'
-      end
+      default['icinga2']['icinga2_version_suffix'] = if node['platform_version'] > '7'
+                                                       '~debmon' + node['platform_version'].split('.')[0] + '+1'
+                                                     else
+                                                       '~debmon' + node['platform_version'].split('.')[0] + '0+1'
+                                                     end
     end
   end
 end
