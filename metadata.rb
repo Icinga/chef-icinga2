5c4e80d3a4f44b90c91aa4dd3f22d2972c06bcf2
Vil Surkin <vill.srk@gmail.com>
2017-07-23 17:11:57 +0300

    fix foodcritic warnings

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@
 name 'icinga2'
 maintainer 'Virender Khatri'
 maintainer_email 'vir.khatri@gmail.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures Icinga2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.9.3'
