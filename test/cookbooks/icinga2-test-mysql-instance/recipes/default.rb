5916369bf9884d83ad0981ee302de2bb6ddc8ff7
Vil Surkin <vill.srk@gmail.com>
2017-07-23 10:34:09 +0300

    add mysql57-community repo on centos platform in tests

--- test/cookbooks/icinga2-test-mysql-instance/recipes/default.rb
+++ test/cookbooks/icinga2-test-mysql-instance/recipes/default.rb
@@ -7,6 +7,18 @@ mysql_version = case node['lsb']['codename']
                 else '5.7'
                 end
 
+if node['platform'] == 'centos'
+  yum_repository 'mysql57-community' do
+    baseurl node['yum']['mysql57-community']['baseurl'] unless node['yum']['mysql57-community']['baseurl'].nil?
+    mirrorlist node['yum']['mysql57-community']['mirrorlist'] unless node['yum']['mysql57-community']['mirrorlist'].nil?
+    description node['yum']['mysql57-community']['description'] unless node['yum']['mysql57-community']['description'].nil?
+    failovermethod node['yum']['mysql57-community']['failovermethod'] unless node['yum']['mysql57-community']['failovermethod'].nil?
+    enabled true
+    gpgcheck true
+    gpgkey node['yum']['mysql57-community']['gpgkey'] unless node['yum']['mysql57-community']['gpgkey'].nil?
+  end
+end
+
 mysql_service 'test-mysql' do
   port '3306'
   version mysql_version
